82cd641fb83a02cded280d7b1415328c2e78e8ee
Mirek33g <133931969+Mirek33g@users.noreply.github.com>
2023-09-03 23:56:09 +0000

    Card and buttons

--- main.py
+++ main.py
@@ -3,26 +3,30 @@ from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE_FONT = ("Arial", 20, "italic")
-WORD_FONT = ("Arial", 60, "bold")
+WORD_FONT = ("Arial", 40, "bold")
 
+#creates window 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=600, height=300, highlightthickness=0)
+#creates first card and text on it
+canvas = Canvas(width=780, height=500, highlightthickness=0)
 card_front = PhotoImage(file="card_front.png")
 card_back = PhotoImage(file="card_back.png")
-canvas.create_image(300, 150, image=card_front)
-language = canvas.create_text(300, 50, text="French", font=LANGUAGE_FONT)
+canvas.create_image(401, 264, image=card_front)
+language = canvas.create_text(401, 75, text="French", font=LANGUAGE_FONT)
+word = canvas.create_text(401, 275, text="word",font= WORD_FONT)
 canvas.grid(column=0, row=0, columnspan=2)
 
-
+#creates red cross button "wrong answer"
 image_wrong = PhotoImage(file="wrong.png")
 button_wrong = Button(image=image_wrong, highlightthickness=0)
 button_wrong.grid(column=0, row=1)
 
+#creates green button "right answer"
 image_right = PhotoImage(file="right.png")
-button_right = Button(image=image_right, highlightthickness=0)
+button_right = Button(image=image_right, highlightthickness= 0)
 button_right.grid(column=1, row=1)
 
 
